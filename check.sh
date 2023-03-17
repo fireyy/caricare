86d892d044fc73334b3f329f92c0d23297f77bc0
fireyy <fireyy@gmail.com>
2023-03-17 17:20:11 +0800

    feat: add local ci check

---
+++ check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# This scripts runs various CI-like checks in a convenient way.
+set -eux
+
+cargo check --workspace --all-targets
+cargo fmt --all -- --check
+cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
+cargo test --workspace --all-targets --all-features
+cargo test --workspace --doc
